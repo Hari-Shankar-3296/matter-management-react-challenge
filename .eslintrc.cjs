d83f118422b2a86284cdeac249e046513a16417b
Harishankar Devaraj <harishankar3296@gmail.com>
2026-02-06 03:35:28 +1100

    lint lint issues with test files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,4 +15,13 @@ module.exports = {
       { allowConstantExport: true },
     ],
   },
+  overrides: [
+    {
+      files: ['*.test.ts', '*.test.tsx'],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
+      },
+    },
+  ],
 }
